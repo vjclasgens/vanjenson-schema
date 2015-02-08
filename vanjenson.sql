aed4747d2c74e2358cf39c42ce56259b9ec89f80
V.J. Clasgens <vj@parkifi.com>
2015-02-08 09:45:55 -0700

    refactored work_event to event

--- vanjenson.sql
+++ vanjenson.sql
@@ -4,8 +4,8 @@
 
 
 -- Table for what is being currently worked on / most recent tag line
-CREATE TABLE working_on(
-	working_on_id bigserial PRIMARY KEY,
+CREATE TABLE event(
+	event_id bigserial PRIMARY KEY,
 	event  character varying(255) NOT NULL,
 	event_time timestamp with time zone DEFAULT current_timestamp
 );
